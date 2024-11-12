fb260b307435ea2776cb54457eaf88604fe21ca4
Mikel Urkiaga <murkiaga001@gmail.com>
2024-11-12 13:05:48 +0100

    jandalo zerbitzuan web ahula jarri

---
+++ jandalo/clean_jandalo.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#docker stop pasapasa_ssh_1 
+docker stop jandalo_web_1
+#docker rm pasapasa_ssh_1 
+docker rm jandalo_web_1
---
+++ jandalo/service/run_jandalo.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker-compose -f "$SERVICES_PATH/jandalo/docker-compose.yml" up -d --build
